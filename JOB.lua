271cf6eaad2eb268a3fd603f867b562f25c46043
DrakeFS <drake.fsx@gmail.com>
2020-09-27 16:22:03 -0400

    Added gear set examples to JOB.lua

--- JOB.lua
+++ JOB.lua
@@ -3,6 +3,60 @@
 -- this lua on.  ie. rename to BLU.lua to     --    Ranger = RNG      Samurai = SAM   Ninja = NIN         Dragoon = DRG      Summoner = SMN
 -- load it for Blue Mage                      --    Blue Mage = BLU   Corsair = COR   Puppetmaster = PUP  Dancer = DNC       Scholar = SCH
 ------------------------------------------------    Geomancer = GEO   Rune Fencer = RUN
+--[[
+    Microsoft Visual Studio Code or Notepad++ are the recommended editors for editing LUA files.
+
+    You can use 
+        //gs export 
+    in game (or just "gs export" from the Windower Console) to export your currently equiped gear to the \Windower\addons\GearSwap\data\export folder, in a file named
+        YourCharactername CurrentDate-CurrentTime.lua
+    from which you can copy into the sets in this lua.  
+    
+    If you want to make changes to existing sets, you can use
+        //gs equip sets.SetName.ModName 
+    for example 
+        //gs equip sets.engaged
+    which will equip yout engaged set (TP set).  At which point you can make any gear changes and then export the new set to update this lua.
+
+
+    An example gear set is provided below to show how a "melee gear set" (you do not want to swap anything that resets TP) is built, .
+
+    sets.engaged = {
+        ammo="Ginsen",
+        head="Sulevia's Mask +2",
+        body="Flamma Korazin +2",
+        hands="Sulev. Gauntlets +2",
+        legs="Flamma Dirs +2",
+        feet="Flam. Gambieras +2",
+        neck="Clotharius Torque",
+        waist={ name="Sailfi Belt +1", augments={'Path: A',}},
+        left_ear="Steelflash Earring",
+        right_ear="Bladeborn Earring",
+        left_ring="Pernicious Ring",
+        right_ring="Petrov Ring",
+        back={ name="Brigantia's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Damage taken-5%',}}
+    }
+
+
+    Another example gear set is provided below to show how a "casting gear set" (where you may want to swap weapons and other gear that will reset your tp) is built.
+    
+    sets.midcast['Healing Magic'] = {
+        main={ name="Queller Rod", augments={'MND+15','Mag. Acc.+15','"Cure" potency +15%',}},
+        sub="Sors Shield",
+        head="Theo. Cap +1",
+        body="Theo. Briault +1",
+        hands="Theophany Mitts",
+        legs="Ebers Pantaloons",
+        feet={ name="Piety Duckbills +1", augments={'Enhances "Afflatus Solace" effect',}},
+        neck={ name="Cleric's Torque", augments={'Path: A',}},
+        waist="Friar's Rope",
+        left_ear="Glorious Earring",
+        right_ear={ name="Nourish. Earring +1", augments={'Path: A',}},
+        left_ring="Lebeche Ring",
+        back="Solemnity Cape",
+    }
+]]
+
 
 function get_sets()
     mote_include_version = 2
@@ -22,7 +76,7 @@ sets.precast.FC = {}  -- Fast Cast Set
 
 sets.precast.WS = {}  -- General Weapon Skill Set
 
--- sets.precast.WS['WS_Name'] = set_combine(sets.precast.WS, {gear goes here}) -- Specific Weapon Skill Set.  Ignore if uneeded, remove the first "--" to activate.
+-- sets.precast.WS['WS Name'] = set_combine(sets.precast.WS, {gear goes here}) -- Specific Weapon Skill Set.  Ignore if uneeded, remove the first "--" to activate.
 
 sets.midcast = {} -- Casting Set
 
