d4a2f85474cedf8d9495c588fe965a1b34e26cee
DrakeFS <drake.fsx@gmail.com>
2024-05-25 00:36:26 -0400

    Create SAM.lua

---
+++ SAM.lua
@@ -0,0 +1,106 @@
+function get_sets()
+    mote_include_version = 2
+    include('Mote-Include.lua')
+end
+
+function job_setup()
+
+end
+
+function user_setup()
+    on_job_change()
+end
+
+function init_gear_sets()
+    gear.BackTP = { name="Smertrios's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Store TP"+10','Phys. dmg. taken-10%',}}
+    gear.BackPWS = { name="Smertrios's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}}
+
+    sets.precast.FC = {}  -- Fast Cast Set
+
+    -- sets.precast.RA = {}  -- Ranged delay reduction.  Ignore if uneeded, remove the first "--" to activate.
+
+    sets.precast.JA["Warding Circle"] = {head="Wakido Kabuto +3"}
+    sets.precast.JA["Third Eye"] = {legs="Sakonji Haidate +3"}
+    sets.precast.JA["Meditate"] = {
+        head="Wakido Kabuto +3",
+        hands="Sakonji kote +3",
+        back=gear.BackTP
+    }
+    sets.precast.JA["Seigan"] = {head="Kasuga Kabuto +3"}
+    sets.precast.JA["Sekkanoki"] = {hands="Kasuga Kote +3"}
+    sets.precast.JA["Blade Bash"] = {hands="Sakonji kote +3"}
+    sets.precast.JA["Sengikori"] = {feet="Kas. Sune-Ate +3"}
+    --sets.precast.JA["Meikyo Shisui"] = {feet="Sakonji sune-ate +3"}
+        --[[sets.precast.JA["Hasso"] = { -- Must stay equiped to gain the benefit
+        hands="Wakido Kote +3",
+        legs="Kasuga Haidate +3",
+        feet="Wakido Sune. +3"
+    }]]
+
+    sets.precast.WS = {
+        ammo="Knobkierrie",
+        head="Mpaca's Cap",
+        body={ name="Nyame Mail", augments={'Path: B',}},
+        hands="Kasuga Kote +3",
+        legs={ name="Nyame Flanchard", augments={'Path: B',}},
+        feet={ name="Nyame Sollerets", augments={'Path: B',}},
+        neck="Rep. Plat. Medal",
+        waist={ name="Sailfi Belt +1", augments={'Path: A',}},
+        left_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
+        right_ear="Thrud Earring",
+        left_ring="Cornelia's Ring",
+        right_ring="Epaminondas's Ring",
+        back=gear.BackPWS
+    }
+
+    -- sets.midcast.RA = {} -- Ranged TP Set.  Ignore if uneeded, remove the first "--" to activate.
+
+    sets.defense.PDT = {
+        ammo="Amar Cluster",
+        head="Mpaca's Cap",
+        body={ name="Nyame Mail", augments={'Path: B',}},
+        hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+        legs={ name="Nyame Flanchard", augments={'Path: B',}},
+        feet={ name="Nyame Sollerets", augments={'Path: B',}},
+        neck="Loricate Torque +1",
+        waist="Plat. Mog. Belt",
+        left_ring="Defending Ring",
+        right_ring="Vocane Ring",
+        back=gear.BackTP
+    }
+
+    sets.idle = {
+        ammo="Amar Cluster",
+        head={ name="Nyame Helm", augments={'Path: B',}},
+        body={ name="Nyame Mail", augments={'Path: B',}},
+        hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+        legs={ name="Nyame Flanchard", augments={'Path: B',}},
+        feet={ name="Nyame Sollerets", augments={'Path: B',}},
+        neck="Loricate Torque +1",
+        waist="Plat. Mog. Belt",
+        left_ring="Defending Ring",
+        right_ring="Vocane Ring",
+        back=gear.BackTP
+    }
+
+    sets.engaged = {
+        ammo={ name="Coiste Bodhar", augments={'Path: A',}},
+        head="Kasuga Kabuto +3",
+        body="Kasuga Domaru +3",
+        hands="Flam. Manopolas +2",
+        legs="Kasuga Haidate +3",
+        feet={ name="Ryuo Sune-Ate", augments={'HP+50','"Store TP"+4','"Subtle Blow"+7',}},
+        neck={ name="Vim Torque +1", augments={'Path: A',}},
+        waist="Sarissapho. Belt",
+        left_ear="Crep. Earring",
+        right_ear="Cessance Earring",
+        left_ring="Petrov Ring",
+        right_ring="Ilabrat Ring",
+        back=gear.BackTP
+    }
+end
+
+function on_job_change()
+    set_macro_page(1, 12)
+    send_command('wait 5;input /lockstyleset 184')
+end
